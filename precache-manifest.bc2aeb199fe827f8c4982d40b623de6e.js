89863019482bb7a77c4a78b0e7fcda54020fb633
anserme <lczr1026673211@gmail.com>
2020-06-01 23:51:29 +0800

    first commit

---
+++ precache-manifest.bc2aeb199fe827f8c4982d40b623de6e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a1dcc58b758cc38a945baff957a70c",
+    "url": "/blk-design-system-react/index.html"
+  },
+  {
+    "revision": "0f413399fdd4819703a9",
+    "url": "/blk-design-system-react/static/css/main.9db742ec.chunk.css"
+  },
+  {
+    "revision": "0592acbf5cfa5b19b402",
+    "url": "/blk-design-system-react/static/js/2.aa6a47d8.chunk.js"
+  },
+  {
+    "revision": "15443a68910946e5c0298d95c5aec6e4",
+    "url": "/blk-design-system-react/static/js/2.aa6a47d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f413399fdd4819703a9",
+    "url": "/blk-design-system-react/static/js/main.5a31dae2.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/blk-design-system-react/static/js/main.5a31dae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b819ec593d41f7951a2e",
+    "url": "/blk-design-system-react/static/js/runtime-main.f6a9f93e.js"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/blk-design-system-react/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/blk-design-system-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/blk-design-system-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/blk-design-system-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/blk-design-system-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/blk-design-system-react/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/blk-design-system-react/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/blk-design-system-react/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/blk-design-system-react/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/blk-design-system-react/static/media/square5.ff004a0e.png"
+  }
+]);
